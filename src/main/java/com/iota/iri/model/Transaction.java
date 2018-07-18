847794212e80421b739b63b4054451d60b6047ac
Hans Moog <hm@mkjc.net>
2018-07-18 02:58:38 +0200

    Feat: added a version byte to the db schema and some comments

--- src/main/java/com/iota/iri/model/Transaction.java
+++ src/main/java/com/iota/iri/model/Transaction.java
@@ -10,7 +10,18 @@ import java.nio.ByteBuffer;
  * Created by paul on 3/2/17 for iri.
  */
 public class Transaction implements Persistable {
+    /**
+     * Size of the raw transaction bytes without the metadata.
+     */
     public static final int SIZE = 1604;
+
+    /**
+     * Database schema version for this entitiy.
+     *
+     * It should be increased whenever we add or remove properties. The readMetadata method reacts to the version flag
+     * and adjusts the parsing mechanism accordingly.
+     */
+    public static final byte SCHEMA_VERSION = 1;
 
     public byte[] bytes;
 
@@ -58,10 +69,10 @@ public class Transaction implements Persistable {
         int allocateSize =
                 Hash.SIZE_IN_BYTES * 6 + //address,bundle,trunk,branch,obsoleteTag,tag
                         Long.BYTES * 9 + //value,currentIndex,lastIndex,timestamp,attachmentTimestampLowerBound,attachmentTimestampUpperBound,arrivalTime,height
-                        Integer.BYTES * 3 + //validity,type,snapshot
+                        Integer.BYTES * 4 + //validity,type,snapshot,referencedSnapshot
                         1 + //solid
                         sender.getBytes().length + //sender
-                        Integer.BYTES; // referenced Snapshot
+                        1; // version
         ByteBuffer buffer = ByteBuffer.allocate(allocateSize);
         buffer.put(address.bytes());
         buffer.put(bundle.bytes());
@@ -87,13 +98,23 @@ public class Transaction implements Persistable {
         buffer.put(Serializer.serialize(snapshot));
         buffer.put(Serializer.serialize(referencedSnapshot));
         buffer.put(sender.getBytes());
+
+        // write the version flag of the transaction (introduced in version 1)
+        buffer.put((byte) (SCHEMA_VERSION & 0b10000000));
+
         return buffer.array();
     }
 
+    /**
+     * This method parses the metadata from the saved bytes of the database.
+     *
+     * @param bytes metadata bytes (different length depending on schema version)
+     */
     @Override
     public void readMetadata(byte[] bytes) {
         int i = 0;
         if(bytes != null) {
+            // the beginning of the database structure is the same among all versions and doesn't change very often
             address = new Hash(bytes, i, Hash.SIZE_IN_BYTES);
             i += Hash.SIZE_IN_BYTES;
             bundle = new Hash(bytes, i, Hash.SIZE_IN_BYTES);
@@ -134,18 +155,36 @@ public class Transaction implements Persistable {
             confirmed = bytes[i] == 1;
             i++;
             */
+
+            // solid flag of the transaction (if all predecessors are known)
             solid = bytes[i] == 1;
             i++;
+
+            // the milestone index that references this transactions (0 if not confirmed yet)
             snapshot = Serializer.getInteger(bytes, i);
             i += Integer.BYTES;
-            referencedSnapshot = Serializer.getInteger(bytes, i);
-            i += Integer.BYTES;
+
+            // determine the version of our schema and do some schema specific parsing
+            // since the schema always ends with a string consisting out of ascii chars we can easily determine if a
+            // version byte exists already by checking if the first bit of the last byte starts with a 1
+            byte schemaVersion = (bytes[bytes.length - 1] & 0b10000000) != 0 ? (byte) (bytes[bytes.length - 1] & 0b01111111) : 0;
+            switch (schemaVersion) {
+                // schema version 1 specific properties
+                case 1:
+                    // the milestone that was directly or indirectly referenced by this transaction
+                    referencedSnapshot = Serializer.getInteger(bytes, i);
+                    i += Integer.BYTES;
+            }
+
+            // the sender bytes are always at the end because they have a dynamic size
             byte[] senderBytes = new byte[bytes.length - i];
             if (senderBytes.length != 0) {
                 System.arraycopy(bytes, i, senderBytes, 0, senderBytes.length);
             }
             sender = new String(senderBytes);
             i += senderBytes.length;
+
+            // transaction was successfully parsed from the database
             parsed = true;
         }
     }
