9d0411082f26087c038f3f8f6a02e527600a70b1
Hans Moog <hm@mkjc.net>
2018-07-26 20:40:22 +0200

    Refactor: added the additional parameter in the remaining classes

--- src/main/java/com/iota/iri/service/tipselection/impl/EntryPointSelectorImpl.java
+++ src/main/java/com/iota/iri/service/tipselection/impl/EntryPointSelectorImpl.java
@@ -30,7 +30,7 @@ public class EntryPointSelectorImpl implements EntryPointSelector {
     public Hash getEntryPoint(int depth) throws Exception {
         int milestoneIndex = Math.max(milestone.latestSolidSubtangleMilestoneIndex - depth - 1, 0);
         MilestoneViewModel milestoneViewModel =
-                MilestoneViewModel.findClosestNextMilestone(tangle, milestoneIndex, testnet, milestoneStartIndex);
+                MilestoneViewModel.findClosestNextMilestone(tangle, milestoneIndex, testnet, milestoneStartIndex, MilestoneViewModel.latest(tangle).index());
         if (milestoneViewModel != null && milestoneViewModel.getHash() != null) {
             return milestoneViewModel.getHash();
         }
