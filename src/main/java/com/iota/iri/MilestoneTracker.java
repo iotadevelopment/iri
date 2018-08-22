48a1993d0f7286ee79100d0ad79b06f8b11814c9
Hans Moog <hm@mkjc.net>
2018-08-22 17:20:08 +0200

    Feat: debug

--- src/main/java/com/iota/iri/MilestoneTracker.java
+++ src/main/java/com/iota/iri/MilestoneTracker.java
@@ -187,6 +187,8 @@ public class MilestoneTracker {
                     } catch(Exception e) {
                         e.printStackTrace();
                     }
+
+                    System.out.println(unsolidMilestones.size());
                 });
 
                 try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
