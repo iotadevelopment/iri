ecb0993da1b70b2131b75db05872edfdbd301fef
Hans Moog <hm@mkjc.net>
2018-09-02 22:53:47 +0200

    Feat: added debug message to milestone solidifer

--- src/main/java/com/iota/iri/MilestoneTracker.java
+++ src/main/java/com/iota/iri/MilestoneTracker.java
@@ -235,6 +235,8 @@ public class MilestoneTracker {
                         else if(entry.getValue() < snapshotManager.getLatestSnapshot().getIndex() + 50) {
                             milestonesProcessed++;
 
+                            System.out.println("Trying to solidify: " + entry.getKey().toString());
+
                             // remove milestones that have become solid
                             if(transactionValidator.checkSolidity(entry.getKey(), true)) {
                                 unsolidMilestones.remove(entry.getKey());
@@ -250,6 +252,7 @@ public class MilestoneTracker {
 
                     // if we didnt process a single milestone but we have an unprocessed one -> try to solidify
                     if(milestonesProcessed == 0 && earlistMilestoneHash != null) {
+                        System.out.println("Trying to solidify: " + earlistMilestoneHash.toString());
                         if(transactionValidator.checkSolidity(earlistMilestoneHash, true)) {
                             unsolidMilestones.remove(earlistMilestoneHash);
                         }
