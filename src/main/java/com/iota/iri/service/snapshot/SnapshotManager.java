13770748492f9d9d38dae50570b35a80d2cd9557
Hans Moog <hm@mkjc.net>
2018-09-14 00:22:21 +0200

    Feat: debug

--- src/main/java/com/iota/iri/service/snapshot/SnapshotManager.java
+++ src/main/java/com/iota/iri/service/snapshot/SnapshotManager.java
@@ -326,6 +326,7 @@ public class SnapshotManager {
             MilestoneViewModel nextMilestone = targetMilestone;
             while(nextMilestone != null && nextMilestone.index() > initialSnapshot.getIndex() && progressLogger.getCurrentStep() < progressLogger.getStepCount()) {
                 MilestoneViewModel currentMilestone = nextMilestone;
+                System.out.println(currentMilestone.getHash());
                 dagHelper.traverseApprovees(
                     currentMilestone.getHash(),
                     currentTransaction -> {
