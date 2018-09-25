8e4d022f06bbc7d77c14c271cde36a221b7708c5
Hans Moog <hm@mkjc.net>
2018-09-26 01:58:08 +0200

    Fix: fixed status message

--- src/main/java/com/iota/iri/service/milestone/MilestoneSolidifier.java
+++ src/main/java/com/iota/iri/service/milestone/MilestoneSolidifier.java
@@ -282,8 +282,8 @@ public class MilestoneSolidifier {
      * @return true if there are no unsolid milestones that have to be processed or if the earliest milestone is solid
      */
     private boolean isSolid(Hash hash) {
-        if (unsolidMilestonesPool.size() > 1 && youngestMilestoneMarker != null) {
-            statusLogger.status("Solidifying milestone #" + unsolidMilestonesPool.get(youngestMilestoneMarker) + " [" + milestonesToSolidify.size() + " / " + unsolidMilestonesPool.size() + "]");
+        if (unsolidMilestonesPool.size() > 1) {
+            statusLogger.status("Solidifying milestone #" + unsolidMilestonesPool.get(hash) + " [" + milestonesToSolidify.size() + " / " + unsolidMilestonesPool.size() + "]");
         }
 
         try {
